744849da15a2ef7f98ef6c0c0381824b62d9eff2
aaadiknh <adiknurhalimah@gmail.com>
2024-07-23 22:33:25 +0700

    Update index.html dan file lainnya

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,6 @@ const navbar = document.querySelector(".navigation");
 const mybutton = document.getElementById("btn-back-to-top");
 const modalSubmission = document.getElementById("modal-submission");
 const modalDaftar = document.getElementById("modal-daftar");
-const modalWebinar = document.getElementById("modal-webinar");
-const modalRegistrasiWebinar = document.getElementById("modal-registrasi-webinar");
 
 const countDownDay = document.getElementById("countDownDay");
 const countDownHour = document.getElementById("countDownHour");
@@ -121,7 +119,7 @@ hamburgerMenu.addEventListener("click", function () {
   }, 100);
 });
 
-const countDownDate = new Date("Nov 26, 2023 00:00:00").getTime();
+const countDownDate = new Date("Juli 30, 2024 00:00:00").getTime();
 const x = setInterval(function () {
   const now = new Date().getTime();
   const distance = countDownDate - now;
@@ -175,43 +173,43 @@ document
   .getElementById("close-button-submission")
   .addEventListener("click", closeModalSubmission);
 
-  const closeModalWebinar = () => {
-	console.log("tes modal sub");
-	modalWebinar.classList.toggle("opacity-100");
-	modalWebinar.classList.toggle("opacity-0");
-	setTimeout(() => {
-	  modalWebinar.classList.toggle("hidden");
-	}, 300);
-  };
+  //const closeModalWebinar = () => {
+	//console.log("tes modal sub");
+	//modalWebinar.classList.toggle("opacity-100");
+	//modalWebinar.classList.toggle("opacity-0");
+	//setTimeout(() => {
+	//  modalWebinar.classList.toggle("hidden");
+	//}, 300);
+  //};
 
-  document
-  .getElementById("backdrop-webinar")
-  .addEventListener("click", closeModalWebinar);
+  //document
+  //.getElementById("backdrop-webinar")
+  //.addEventListener("click", closeModalWebinar);
 
-  const closeModalRegistrasiWebinar = () => {
-	modalRegistrasiWebinar.classList.toggle("opacity-100");
-	modalRegistrasiWebinar.classList.toggle("opacity-0");
-	setTimeout(() => {
-	  modalRegistrasiWebinar.classList.toggle("hidden");
-	}, 300);
-  };
+  //const closeModalRegistrasiWebinar = () => {
+	//modalRegistrasiWebinar.classList.toggle("opacity-100");
+	//modalRegistrasiWebinar.classList.toggle("opacity-0");
+	//setTimeout(() => {
+	//  modalRegistrasiWebinar.classList.toggle("hidden");
+	//}, 300);
+  //};
 
-  document
-  .getElementById("backdrop-registrasi-webinar")
-  .addEventListener("click", closeModalRegistrasiWebinar);
-document
-  .getElementById("close-button-registrasi-webinar")
-  .addEventListener("click", closeModalRegistrasiWebinar);
+  //document
+  //.getElementById("backdrop-registrasi-webinar")
+  //.addEventListener("click", closeModalRegistrasiWebinar);
+//document
+  //.getElementById("close-button-registrasi-webinar")
+  //.addEventListener("click", closeModalRegistrasiWebinar);
 
-function moreInfoWebinar() {
-	event.preventDefault();
+//function moreInfoWebinar() {
+	//event.preventDefault();
   
-	modalWebinar.classList.toggle("hidden");
-	setTimeout(() => {
-	  modalWebinar.classList.toggle("opacity-0");
-	  modalWebinar.classList.toggle("opacity-100");
-	}, 100);
-  }
+	//modalWebinar.classList.toggle("hidden");
+	//setTimeout(() => {
+	 // modalWebinar.classList.toggle("opacity-0");
+	 // modalWebinar.classList.toggle("opacity-100");
+	//}, 100);
+  //}
 
 function goToGuidebook(lomba) {
 	event.preventDefault();
@@ -239,15 +237,15 @@ function daftarLomba(lomba) {
   }, 100);
 }
 
-function daftarWebinar() {
-	event.preventDefault();
+//function daftarWebinar() {
+	//event.preventDefault();
   
-	modalRegistrasiWebinar.classList.toggle("hidden");
-	setTimeout(() => {
-	  modalRegistrasiWebinar.classList.toggle("opacity-0");
-	  modalRegistrasiWebinar.classList.toggle("opacity-100");
-	}, 100);
-  }
+	//modalRegistrasiWebinar.classList.toggle("hidden");
+	//setTimeout(() => {
+	  //modalRegistrasiWebinar.classList.toggle("opacity-0");
+	  //modalRegistrasiWebinar.classList.toggle("opacity-100");
+	//}, 100);
+  //}
 
 function submissionLomba() {
   event.preventDefault();
