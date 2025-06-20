545387e9ea03aea0d7f5b2c581c2e12727a8b133
maroisa <ahmadroisul412@gmail.com>
2025-06-20 12:47:20 +0700

    menghilangkan text shadow untuk optimisasi

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import tailwindcss from "@tailwindcss/vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), svelte()],
-  server: {
-    port: 3000
-  }
+    plugins: [tailwindcss(), svelte()],
 })
